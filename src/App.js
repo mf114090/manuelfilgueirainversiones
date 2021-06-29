bfede34befbd7567d81a8d56c0aad923e87cf5d8
Manuel Filgueira Marin <79112241+mf114090@users.noreply.github.com>
2021-06-29 19:09:06 -0400

    componentesfuncionando

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Route } from "react-router-dom";
+import Contact from './Contact';
+import About from './About';
+import Home from './Home';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = ()=> {
+    return (
+        <BrowserRouter>
+            <Route exact path="/Contact" component ={Contact} />
+            <Route exact path="/About" component ={About} />
+            <Route exact path="/">
+                <Home />
+            </Route>
+        
+        
+        
+        </BrowserRouter>
+    )
 }
 
 export default App;
