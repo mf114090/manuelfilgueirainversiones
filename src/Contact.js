743eaf0de8b8e6585f0d0c1d8decbed481b6ebd9
Manuel Filgueira Marin <manuelfilgueira@gmail.com>
2021-10-22 22:01:34 +0000

    linkdefacebook terminado

--- src/Contact.js
+++ src/Contact.js
@@ -1,30 +1,50 @@
 import { Link } from 'react-router-dom'; 
 
+
 import Whatsapp from "./img/whatsapp.png";
 import Instagram from "./img/instagram.png";
+import Facebook from "./img/facebook.png";
 
 const Contact = ()=> {
     return (
-        <div classname= "container">
-            <div classname= "row row-cols-4 row-cols-md-4 ">
-                <div classname="col">
-                    <div classname="icono-whatsapp">
+        <div className= "container">
+            <div className= "row row-cols-4 row-cols-md-4 ">
+                <div className="col">
+                    <div className="fab fa-instagram">
                         <a href="https://wa.me/56964351264">
                             <img src={Whatsapp} className="d-block w-20"   alt="..."/>
                             
                         </a> 
-                    </div> 
-                
-              
-                    <div classname="col">
-                        <div classname="icono-instagram">
-                            <a href="https://www.instagram.com/rentahousechile.oficial">
-                                <img src={Instagram} className="d-block w-20"   alt="..."/>
-                                
-                            </a> 
-                        </div> 
+                       
                     </div>
                 </div> 
+                
+              
+                <div classname="col">
+                    <div classname="icono-instagram">
+                        <a href="https://www.instagram.com/rentahousechile.oficial">
+                                <img src={Instagram} className="d-block w-20"   alt="..."/>
+                                
+                        </a> 
+                    </div> 
+                </div>        
+               
+
+                <div classname="col">
+                     <div classname="icono-instagram">
+                        <a href="https://www.facebook.com/Rent-A-House-Chile-3496566590398567">
+                            <img src={Facebook} className="d-block w-20"   alt="..."/>
+                                
+                        </a> 
+                    </div> 
+                </div>
+               
+            
+            
+            
+            
+            
+            
             </div>  
 
 
