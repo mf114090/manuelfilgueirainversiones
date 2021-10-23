93cb58fe4cb08edc9eefae2eee33bb90603c5bed
Manuel Filgueira Marin <manuelfilgueira@gmail.com>
2021-10-23 14:22:22 +0000

    modificacion de contactos

--- src/Contact.js
+++ src/Contact.js
@@ -1,60 +1,59 @@
 import { Link } from 'react-router-dom'; 
 
 
-import Whatsapp from "./img/whatsapp.png";
+import Imagenwhatsapp from "./img/imagenwhatsapp.png";
 import Instagram from "./img/instagram.png";
 import Facebook from "./img/facebook.png";
 
-const Contact = ()=> {
+const Contact = () => {
     return (
-        <div className= "container">
-            <div className= "row row-cols-4 row-cols-md-4 ">
+        <div classNameName="container">
+            <div classNameName="row row-cols-1 row-cols-md-3 g-4">
                 <div className="col">
-                    <div className="fab fa-instagram">
-                        <a href="https://wa.me/56964351264">
-                            <img src={Whatsapp} className="d-block w-20"   alt="..."/>
-                            
-                        </a> 
-                       
+                    <div classNameName="card">
+                        <img src="Imagenwhatsapp" className="card-img-top" alt="..."/>
+                        <div className ="card-body">
+                        <h5 className ="card-title">Card title</h5>
+                        <p className ="card-text">This is a longer card with supporting text below as a natural lead-in to additional content.This content is a little bit longer.</p>
+                        </div>
                     </div>
-                </div> 
-                
-              
-                <div classname="col">
-                    <div classname="icono-instagram">
-                        <a href="https://www.instagram.com/rentahousechile.oficial">
-                                <img src={Instagram} className="d-block w-20"   alt="..."/>
-                                
-                        </a> 
-                    </div> 
-                </div>        
-               
-
-                <div classname="col">
-                     <div classname="icono-instagram">
-                        <a href="https://www.facebook.com/Rent-A-House-Chile-3496566590398567">
-                            <img src={Facebook} className="d-block w-20"   alt="..."/>
-                                
-                        </a> 
-                    </div> 
                 </div>
-               
-            
-            
-            
-            
-            
-            
-            </div>  
+                <div className="col">
+                    <div className="card">
+                        <img src="Instagram" className="card-img-top" alt="..."/>
+                        <div className ="card-body">
+                        <h5 className ="card-title">Card title</h5>
+                        <p className ="card-text">This is a longer card with supporting text below as a natural lead-in to additional content.This content is a little bit longer.</p>
+                        </div>
+                    </div>
+                </div>
+                <div className="col">
+                    <div className="card">
+                        <img src="..." className="card-img-top" alt="..."/>
+                        <div className ="card-body">
+                        <h5 className ="card-title">Card title</h5>
+                        <p className ="card-text">This is a longer card with supporting text below as a natural lead-in to additional content.</p>
+                        </div>
+                    </div>
+                </div>
+                <div className="col">
+                    <div className="card">
+                        <img src="..." className="card-img-top" alt="..."/>
+                        <div className ="card-body">
+                        <h5 className ="card-title">Card title</h5>
+                        <p className ="card-text">This is a longer card with supporting text below as a natural lead-in to additional content.This content is a little bit longer.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
 
 
 
 
 
-                
 
 
-            
+
         </div>
     )
 }
