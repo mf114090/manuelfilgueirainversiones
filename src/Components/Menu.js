581b13eb8ca42797b984e9afe37eec6d520a0857
Manuel Filgueira Marin <manuelfilgueira@gmail.com>
2022-01-08 21:32:09 +0000

    arreglos menores en menu

--- src/Components/Menu.js
+++ src/Components/Menu.js
@@ -14,24 +14,36 @@ const Menu = () => {
 				<div className="ml-auto"> 
 				
 					<Link to="/">
-					<span className="text-dark mb-0 fs-5 p-3">Inicio</span>
-						
+						<span className="text-dark mb-0 fs-5 p-3">Inicio</span>
 					</Link> 
 					
+					<Link to="/about">
+						<span className="text-dark mb-0 fs-5 p-3">Asesorias</span>
+					</Link> 
+					
+					<Link to="/about">
+						<span className="text-dark mb-0 fs-5 p-3">Propiedades</span>
+					</Link> 
+					
+					
+					<Link to="/about">
+						<span className="text-dark mb-0 fs-5 p-3">Nosotros</span>
+					</Link> 
+					
+					<Link to="/news">				
+						<span className=" text-dark mb-0 fs-5 p-4">Noticias</span>
+					</Link>  
+					
+					<Link to= "/contact">				
+						<span className=" text-dark mb-0 fs-5 p-3">Contacto</span>
+					</Link>
 					
 					{/* <Link to="/Login">
 						<span className="text-dark mb-0 fs-4">log in</span>
 					</Link> | {" "} */}
-					<Link to="/about">
-						<span className="text-dark mb-0 fs-5 p-3">Nosotros</span>
-					</Link> 
-					<Link to= "/contact">				
-						<span className=" text-dark mb-0 fs-5 p-3">Contacto</span>
-					</Link>
-					<Link to="/news">				
-						<span className=" text-dark mb-0 fs-5 p-4">Noticias</span>
-					</Link>  
-                </div>
+					
+                
+				</div>
             </div>
         </div>
     );
